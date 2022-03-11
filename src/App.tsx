499a3a65fd89b276be1ff30d2a5ffec989db0870
Abdul Rehman Shaikh <CORP\2202-001-ars@a-hw0jpshz94r.corp.amazonworkspaces.com>
2022-03-11 10:19:27 +0500

    added baby names

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,47 @@
-import { greet } from "./utils/greet";
+import babydata from "./babynamesdata.json";
+import { useState } from "react";
+
+const maleNames = babydata.filter((x) => x.sex === "m");
+const femaleNames = babydata.filter((x) => x.sex === "f");
+const anyNames = babydata;
+
+interface Properties {
+  id: number;
+  name: string;
+  sex: string;
+}
 
 function App(): JSX.Element {
-  return <h1>{greet("World")}</h1>;
+  const [Names, setNames] = useState<Properties[]>([]);
+
+  const maleClick = () => setNames(maleNames);
+  const femaleClick = () => setNames(femaleNames);
+  const anyClick = () => setNames(anyNames);
+
+  return (
+    <>
+      <h1>Baby Names Data</h1>
+      <hr />
+      <h3>Please Select the sex of your baby</h3>
+
+      <button onClick={maleClick} className={"button"}>
+        Male
+      </button>
+      <button onClick={femaleClick} className={"button"}>
+        Female
+      </button>
+      <button onClick={anyClick} className={"button"}>
+        Any
+      </button>
+      <main className="Main">
+        {Names.map((x, id) => (
+          <li key={x.id} className={x.sex}>
+            {x.name}
+          </li>
+        ))}
+      </main>
+    </>
+  );
 }
 
 export default App;
